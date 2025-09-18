87cf453c7a852a9b5b293968e306c03c127e7ef5
poveteen <poveteen@gmail.com>
2025-09-18 17:27:32 +0900

    Update CreditCardUtil.kt
    pattern update

--- CardScanner/src/main/java/com/example/cardscanner/util/CreditCardUtil.kt
+++ CardScanner/src/main/java/com/example/cardscanner/util/CreditCardUtil.kt
@@ -44,7 +44,8 @@ internal fun String.getCardType(): CardType {
 
 private val VISA = CardType("Visa", R.drawable.icon_visa, creditCardNumberLength = 16)
 private val MASTERCARD = CardType("MasterCard", R.drawable.icon_mastercard, creditCardNumberLength = 16)
-private val AMEX = CardType("Amex", R.drawable.icon_american_express, creditCardNumberLength = 16)
+//private val AMEX = CardType("Amex", R.drawable.icon_american_express, creditCardNumberLength = 16)
+private val AMEX = CardType("Amex", R.drawable.icon_american_express, creditCardNumberLength = 15)
 private val DINER_CLUB = CardType("DinerClub", R.drawable.icon_diner_club, creditCardNumberLength = 14)
 private val DISCOVER = CardType("Discover", R.drawable.icon_discover, creditCardNumberLength = 16)
 private val JCB = CardType("Jcb", R.drawable.icon_jcb, creditCardNumberLength = 16)
@@ -61,7 +62,7 @@ private val SOLO = CardType("Solo", R.drawable.ic_card_grey)
 private val UNKNOWN = CardType("Unknown", R.drawable.ic_card_grey)
 
 private fun listOfPattern() = ArrayList<String>().apply {
-
+/*
     add("^4[0-9]{6,}$") // visa-0
     add("^5[1-5][0-9]{5,}$")  //MasterCard-1
     add("^3[47][0-9]{5,}$")  //AmeExp-2
@@ -80,6 +81,26 @@ private fun listOfPattern() = ArrayList<String>().apply {
     add("^(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}\$") // Switch-13
     add("^(62[0-9]{14,17})$") // Union pay-14
     add("^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$") // Visa Master card-15
+*/
+
+    add("^4[0-9]{12}(?:[0-9]{3})?$")      // Visa: 13자리(구형) 또는 16자리(현행)만 허용
+    add("^5[1-5][0-9]{14}$")              // MasterCard: 51~55, 16자리만
+    add("^3[47][0-9]{13}$")               // Amex: 15자리만
+    add("^3(?:0[0-5]|[68][0-9])[0-9]{11}$") // Diners: 14자리만
+    add("^6(?:011|5[0-9]{2})[0-9]{12}$")  // Discover: 6011/65, 16자리
+    add("^(?:2131|1800)[0-9]{11}$")       // JCB(15자리)
+    add("^35[0-9]{14}$")                  // JCB(16자리, 35로 시작)
+    // 이하 기타카드 패턴은 필요시 최신 BIN 참고
+    add("^(6541|6556)[0-9]{12}$")         // BCGlobal-6
+    add("^389[0-9]{11}$")                 // Carte Blanche-7
+    add("^63[7-9][0-9]{13}$")             // Insta Payment-8
+    add("^9[0-9]{15}$")                   // Korean Local-9
+    add("^(6304|6706|6709|6771)[0-9]{12,15}$") // Laser-10
+    add("^(5018|5020|5038|5893|6304|6759|6761|6762|6763)[0-9]{8,15}$") // Maestro-11
+    add("^(6334|6767)[0-9]{12}$|^(6334|6767)[0-9]{14}$|^(6334|6767)[0-9]{15}$") // Solo-12
+    add("^(4903|4905|4911|4936|6333|6759)[0-9]{12}$|^(4903|4905|4911|4936|6333|6759)[0-9]{14}$|^(4903|4905|4911|4936|6333|6759)[0-9]{15}$|^564182[0-9]{10}$|^564182[0-9]{12}$|^564182[0-9]{13}$|^633110[0-9]{10}$|^633110[0-9]{12}$|^633110[0-9]{13}$") // Switch-13
+    add("^62[0-9]{14,17}$")               // Union pay-14
+    add("^(4[0-9]{15}|5[1-5][0-9]{14})$") // Visa/Master 혼용 BIN - 16자리만 허용
 
 }
 
